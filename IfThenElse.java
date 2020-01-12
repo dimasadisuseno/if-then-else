2b4677e4fa085f2fa7f52e8a73cc722c55418ce3
dimasadisuseno <57306195+dimasadisuseno@users.noreply.github.com>
2020-01-12 15:54:47 +0700

    Add files via upload

---
+++ IfThenElse.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgif.then.pkgelse;
+
+/**
+ *
+ * @author dimaspc
+ */
+public class IfThenElse {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int gaji = 29000000;
+        if ( gaji > 30000000) {
+        System.out.println("andadirektur");} 
+        else if (gaji > 20000000) {
+        System.out.println("andamanager"); }
+        else if (gaji > 10000000) {
+        System.out.println("andakepalabagian"); }
+        else if (gaji > 7000000) {
+        System.out.println("anda karyawan produksi"); }
+    }
+}
+
+        
+     
